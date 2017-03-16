cb65828d88002902fd8eb15ae85c7f4eccdd5706
SonYoungjae <yjson@mdex.co.kr>
2017-03-17 08:17:46 +0900

    up

--- app/src/main/java/com/nordicsemi/venusAlpha01a/TEST.java
+++ app/src/main/java/com/nordicsemi/venusAlpha01a/TEST.java
@@ -1,14 +1,13 @@
 package com.nordicsemi.venusAlpha01a;
 
 /**
- * Created by 권환진 on 2017-03-16.
+ * Created by 손영재 on 2017-03-16.
  */
 
 public class TEST {
 
      private int testNum = 0;
      private String testString = "Hello";
-
 
 
 
